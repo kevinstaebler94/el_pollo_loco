ea67054cb9e25562c9e5ac7c562b6598b98663a7
Kevin <kevinstaebler1994@outlook.de>
2025-06-30 20:56:15 +0200

    created folder structure + added first functions

---
+++ models/moveable-object.class.js
@@ -0,0 +1,19 @@
+class MoveableObject {
+  x = 120;
+  y = 250;
+  img;
+  height = 150;
+  width = 100;
+
+  //loadImage("img/test.png");
+  loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
+
+  moveRight() {
+    console.log("Moving right");
+  }
+
+  moveLeft() {}
+}
