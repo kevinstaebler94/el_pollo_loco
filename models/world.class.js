30258ceecccc0d54cd46d692c2bb87e697739a98
Kevin <kevinstaebler1994@outlook.de>
2025-07-02 21:31:38 +0200

    added more classes

--- models/world.class.js
+++ models/world.class.js
@@ -1,6 +1,13 @@
 class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
+  clouds = [new Cloud()];
+  backgroundObjects = [
+    new BackgroundObject("img/5_background/layers/air.png", 0),
+    new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),
+    new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 0),
+    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),
+  ];
   canvas;
   ctx;
 
@@ -12,28 +19,32 @@ class World {
 
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.ctx.drawImage(
-      this.character.img,
-      this.character.x,
-      this.character.y,
-      this.character.width,
-      this.character.height
-    );
 
-    this.enemies.forEach((enemy) => {
-      this.ctx.drawImage(
-        enemy.img,
-        enemy.x,
-        enemy.y,
-        enemy.width,
-        enemy.height
-      );
-    });
+    this.addObjectsToMap(this.backgroundObjects);
+    this.addToMap(this.character);
+    this.addObjectsToMap(this.enemies);
+    this.addObjectsToMap(this.clouds);
 
     // Draw() wird immer wieder aufgerufen
     let self = this;
     requestAnimationFrame(() => {
       self.draw();
     });
+  }
+
+  addObjectsToMap(objects) {
+    objects.forEach((object) => {
+      this.addToMap(object);
+    });
+  }
+
+  addToMap(moveableObject) {
+    this.ctx.drawImage(
+      moveableObject.img,
+      moveableObject.x,
+      moveableObject.y,
+      moveableObject.width,
+      moveableObject.height
+    );
   }
 }
