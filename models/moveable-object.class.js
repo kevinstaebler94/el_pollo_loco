87e7e49b8a5d30b2fc7d08131f979cc7c0015d1c
Kevin <kevinstaebler1994@outlook.de>
2025-07-09 22:07:19 +0200

    added animation to clouds

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -2,11 +2,26 @@ class MoveableObject {
   x = 120;
   y = 285;
   img;
+  height = 150;
+  width = 100;
+  imageCache = {};
 
   //loadImage("img/test.png");
   loadImage(path) {
     this.img = new Image();
     this.img.src = path;
+  }
+
+  /**
+   *
+   * @param {Array} arr - ["img/image1.png", "img/image2.png", ...]
+   */
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = img;
+    });
   }
 
   moveRight() {
