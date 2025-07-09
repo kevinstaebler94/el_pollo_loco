19d2737340f7a2c6cdac6c30edef45dddee2ec44
Kevin <kevinstaebler1994@outlook.de>
2025-07-09 22:07:19 +0200

    added animation to clouds

--- models/character.class.js
+++ models/character.class.js
@@ -3,9 +3,30 @@ class Character extends MoveableObject {
   width = 150;
   y = 145;
   x = 20;
+  IMAGES_WALKING = [
+    "img/2_character_pepe/2_walk/W-21.png",
+    "img/2_character_pepe/2_walk/W-22.png",
+    "img/2_character_pepe/2_walk/W-23.png",
+    "img/2_character_pepe/2_walk/W-24.png",
+    "img/2_character_pepe/2_walk/W-25.png",
+    "img/2_character_pepe/2_walk/W-26.png",
+  ];
+
+  currentImage = 0;
 
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
+    this.loadImages(this.IMAGES_WALKING);
+
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      let path = this.IMAGES_WALKING[this.currentImage];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 1000);
   }
 
   jump() {}
