3a7941504ed7926658ffbc065a9cc43cd8e112b8
Kevin <kevinstaebler1994@outlook.de>
2025-07-10 11:45:07 +0200

    added function to animate chicken

--- models/character.class.js
+++ models/character.class.js
@@ -12,8 +12,6 @@ class Character extends MoveableObject {
     "img/2_character_pepe/2_walk/W-26.png",
   ];
 
-  currentImage = 0;
-
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
     this.loadImages(this.IMAGES_WALKING);
@@ -23,10 +21,12 @@ class Character extends MoveableObject {
 
   animate() {
     setInterval(() => {
-      let path = this.IMAGES_WALKING[this.currentImage];
+      let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 7 % 6 => 1, Rest 1
+      // i = 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0 ...
+      let path = this.IMAGES_WALKING[i];
       this.img = this.imageCache[path];
       this.currentImage++;
-    }, 1000);
+    }, 100);
   }
 
   jump() {}
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,10 +2,28 @@ class Chicken extends MoveableObject {
   height = 80;
   width = 80;
   y = 355;
+  IMAGES_WALKING = [
+    "img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
+    "img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
+    "img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
+  ];
 
   constructor() {
     super().loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
+    this.loadImages(this.IMAGES_WALKING);
 
     this.x = 300 + Math.random() * 500;
+    this.animate();
+  }
+
+  animate() {
+    this.moveLeft();
+
+    setInterval(() => {
+      let i = this.currentImage % this.IMAGES_WALKING.length;
+      let path = this.IMAGES_WALKING[i];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 200);
   }
 }
