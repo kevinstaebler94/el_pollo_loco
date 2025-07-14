fb644a9df21e9b598350e4035887be4114473237
Kevin <kevinstaebler1994@outlook.de>
2025-07-14 23:34:53 +0200

    added function to move character left

--- js/games.js
+++ js/games.js
@@ -10,59 +10,45 @@ function init() {
 }
 
 window.addEventListener("keydown", (e) => {
-  console.log(e);
-
   if (e.keyCode == 38) {
     keyboard.UP = true;
-    console.log(keyboard.UP);
   }
 
   if (e.keyCode == 39) {
     keyboard.RIGHT = true;
-    console.log(keyboard.RIGHT);
   }
 
   if (e.keyCode == 40) {
     keyboard.DOWN = true;
-    console.log(keyboard.DOWN);
   }
 
   if (e.keyCode == 37) {
     keyboard.LEFT = true;
-    console.log(keyboard.LEFT);
   }
 
   if (e.keyCode == 32) {
     keyboard.SPACE = true;
-    console.log(keyboard.SPACE);
   }
 });
 
 window.addEventListener("keyup", (e) => {
-  console.log(e);
-
   if (e.keyCode == 38) {
     keyboard.UP = false;
-    console.log(keyboard.UP);
   }
 
   if (e.keyCode == 39) {
     keyboard.RIGHT = false;
-    console.log(keyboard.RIGHT);
   }
 
   if (e.keyCode == 40) {
     keyboard.DOWN = false;
-    console.log(keyboard.DOWN);
   }
 
   if (e.keyCode == 37) {
     keyboard.LEFT = false;
-    console.log(keyboard.LEFT);
   }
 
   if (e.keyCode == 32) {
     keyboard.SPACE = false;
-    console.log(keyboard.SPACE);
   }
 });
