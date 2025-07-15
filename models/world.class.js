2f2aa2a64873282e5e489f95aea81a266c5940da
Kevin <kevinstaebler1994@outlook.de>
2025-07-15 17:26:32 +0200

    added function to change the direction of the character

--- models/world.class.js
+++ models/world.class.js
@@ -1,16 +1,12 @@
 class World {
   character = new Character();
-  enemies = [new Chicken(), new Chicken(), new Chicken()];
-  clouds = [new Cloud()];
-  backgroundObjects = [
-    new BackgroundObject("img/5_background/layers/air.png", 0),
-    new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),
-    new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 0),
-    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),
-  ];
+  enemies = level1.enemies;
+  clouds = level1.clouds;
+  backgroundObjects = level1.backgroundObjects;
   canvas;
   ctx;
   keyboard;
+  camera_x = 0;
 
   constructor(canvas, keyboard) {
     this.ctx = canvas.getContext("2d");
@@ -27,10 +23,15 @@ class World {
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+    this.ctx.translate(this.camera_x, 0);
+
     this.addObjectsToMap(this.backgroundObjects);
+
     this.addToMap(this.character);
     this.addObjectsToMap(this.enemies);
     this.addObjectsToMap(this.clouds);
+
+    this.ctx.translate(-this.camera_x, 0);
 
     // Draw() wird immer wieder aufgerufen
     let self = this;
@@ -46,6 +47,12 @@ class World {
   }
 
   addToMap(moveableObject) {
+    if (moveableObject.otherDirection) {
+      this.ctx.save();
+      this.ctx.translate(moveableObject.width, 0);
+      this.ctx.scale(-1, 1);
+      moveableObject.x = moveableObject.x * -1;
+    }
     this.ctx.drawImage(
       moveableObject.img,
       moveableObject.x,
@@ -53,5 +60,9 @@ class World {
       moveableObject.width,
       moveableObject.height
     );
+    if (moveableObject.otherDirection) {
+      moveableObject.x = moveableObject.x * -1;
+      this.ctx.restore();
+    }
   }
 }
