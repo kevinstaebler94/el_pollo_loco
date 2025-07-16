f395070b08d77542f4ae2df911dfc14122a2bd3a
Kevin <kevinstaebler1994@outlook.de>
2025-07-16 20:58:06 +0200

    added function to let the character jump

--- models/character.class.js
+++ models/character.class.js
@@ -1,7 +1,7 @@
 class Character extends MoveableObject {
   height = 300;
   width = 150;
-  y = 45;
+  y = 55;
   x = 20;
   speed = 10;
   IMAGES_WALKING = [
@@ -12,11 +12,23 @@ class Character extends MoveableObject {
     "img/2_character_pepe/2_walk/W-25.png",
     "img/2_character_pepe/2_walk/W-26.png",
   ];
+  IMAGES_JUMPING = [
+    "img/2_character_pepe/3_jump/J-31.png",
+    "img/2_character_pepe/3_jump/J-32.png",
+    "img/2_character_pepe/3_jump/J-33.png",
+    "img/2_character_pepe/3_jump/J-34.png",
+    "img/2_character_pepe/3_jump/J-35.png",
+    "img/2_character_pepe/3_jump/J-36.png",
+    "img/2_character_pepe/3_jump/J-37.png",
+    "img/2_character_pepe/3_jump/J-38.png",
+    "img/2_character_pepe/3_jump/J-39.png",
+  ];
   world;
 
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
     this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_JUMPING);
     this.applyGravitiy();
     this.animate();
   }
@@ -24,18 +36,30 @@ class Character extends MoveableObject {
   animate() {
     setInterval(() => {
       if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
-        this.x += this.speed;
-        this.otherDirection = false;
+        this.moveRight();
+        // this.walking_sound.play();
       }
 
       if (this.world.keyboard.LEFT && this.x > 0) {
-        this.x -= this.speed;
-        this.otherDirection = true;
+        this.moveLeft();
+        // this.walking_sound.play();
       }
+
+      // console.log("this.speedY", this.speedY);
+
+      if (this.world.keyboard.SPACE && !this.isAboveGround()) {
+        this.jump();
+      }
+
       this.world.camera_x = -this.x + 50;
     }, 1000 / 60);
 
     setInterval(() => {
+      if (this.isAboveGround()) {
+        //Jump animation
+        this.playAnimation(this.IMAGES_JUMPING);
+      }
+
       if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
         // Walk animation
         this.playAnimation(this.IMAGES_WALKING);
@@ -43,5 +67,7 @@ class Character extends MoveableObject {
     }, 100);
   }
 
-  jump() {}
+  jump() {
+    this.speedY = 30;
+  }
 }
