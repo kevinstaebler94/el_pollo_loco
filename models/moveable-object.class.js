516f84fe908e4f38888dc7175353932a3bc72798
Kevin <kevinstaebler1994@outlook.de>
2025-07-21 21:03:41 +0200

    added drawable-object and statusbar to the game

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -1,11 +1,4 @@
-class MoveableObject {
-  x = 120;
-  y = 285;
-  img;
-  height = 150;
-  width = 100;
-  imageCache = {};
-  currentImage = 0;
+class MoveableObject extends DrawableObject {
   speed = 0.15;
   otherDirection = false;
   speedY = 0;
@@ -24,38 +17,6 @@ class MoveableObject {
 
   isAboveGround() {
     return this.y < 140;
-  }
-
-  //loadImage("img/test.png");
-  loadImage(path) {
-    this.img = new Image();
-    this.img.src = path;
-  }
-
-  /**
-   *
-   * @param {Array} arr - ["img/image1.png", "img/image2.png", ...]
-   */
-  loadImages(arr) {
-    arr.forEach((path) => {
-      let img = new Image();
-      img.src = path;
-      this.imageCache[path] = img;
-    });
-  }
-
-  draw(ctx) {
-    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-  }
-
-  drawFrame(ctx) {
-    if (this instanceof Character || this instanceof Chicken) {
-      ctx.lineWidth = "2";
-      ctx.beginPath();
-      ctx.strokeStyle = "blue";
-      ctx.rect(this.x, this.y, this.width, this.height);
-      ctx.stroke();
-    }
   }
 
   // character.isColliding(chicken);
